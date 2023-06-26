09695d098ae0425cb5e4d4a33cab3fca39d30026
Ioannis Valasakis <code@wizofe.uk>
2023-06-26 13:35:46 +0300

    Enhance layout

--- pygui/main.py
+++ pygui/main.py
@@ -1,5 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QComboBox
-
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QComboBox, QLabel, QLineEdit
 from trainer import train_and_evaluate
 from model import Net, ConvNet
 
@@ -10,24 +9,50 @@ class MainWindow(QWidget):
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
-        self.combo = QComboBox()
-        self.combo.addItem("Net")
-        self.combo.addItem("ConvNet")
-        self.layout.addWidget(self.combo)
+        self.model_label = QLabel('Select Model')
+        self.layout.addWidget(self.model_label)
 
-        self.button = QPushButton('Train')
-        self.button.clicked.connect(self.on_button_click)
-        self.layout.addWidget(self.button)
+        self.model_combo = QComboBox()
+        self.model_combo.addItem("Net")
+        self.model_combo.addItem("ConvNet")
+        self.layout.addWidget(self.model_combo)
 
-    def on_button_click(self):
-        model_name = self.combo.currentText()
+        self.dataset_label = QLabel('Select Dataset')
+        self.layout.addWidget(self.dataset_label)
+
+        self.dataset_combo = QComboBox()
+        self.dataset_combo.addItem("MNIST")
+        self.dataset_combo.addItem("TimeSeries")
+        self.layout.addWidget(self.dataset_combo)
+
+        self.learning_rate_label = QLabel('Learning Rate')
+        self.layout.addWidget(self.learning_rate_label)
+
+        self.learning_rate_edit = QLineEdit()
+        self.layout.addWidget(self.learning_rate_edit)
+
+        self.epoch_label = QLabel('Number of Epochs')
+        self.layout.addWidget(self.epoch_label)
+
+        self.epoch_edit = QLineEdit()
+        self.layout.addWidget(self.epoch_edit)
+
+        self.train_button = QPushButton('Train')
+        self.train_button.clicked.connect(self.on_train_button_click)
+        self.layout.addWidget(self.train_button)
+
+    def on_train_button_click(self):
+        model_name = self.model_combo.currentText()
+        dataset_name = self.dataset_combo.currentText()
+        learning_rate = float(self.learning_rate_edit.text())
+        num_epochs = int(self.epoch_edit.text())
 
         if model_name == "Net":
             model = Net(784, 500, 10)
         elif model_name == "ConvNet":
             model = ConvNet(10)
 
-        train_and_evaluate(model)
+        train_and_evaluate(model, dataset_name, learning_rate, num_epochs)
 
 if __name__ == "__main__":
     app = QApplication([])
